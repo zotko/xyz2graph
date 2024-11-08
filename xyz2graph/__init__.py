cb15ce74751beacede107a8e11220a10545338d2
Mykola Zotko <mzotko@gmail.com>
2024-11-08 09:15:09 +0100

    Refactor deprecate functions to helpers module

--- xyz2graph/__init__.py
+++ xyz2graph/__init__.py
@@ -1,31 +1,5 @@
-import warnings
-
-import networkx as nx
-import plotly.graph_objs as go
-
+from .helpers import to_networkx_graph, to_plotly_figure
 from .xyz2graph import DEFAULT_CPK_COLORS, DEFAULT_RADII, MolGraph
-
-
-def to_networkx_graph(graph: MolGraph) -> nx.Graph:
-    """Deprecated: Use MolGraph.to_networkx() method instead."""
-    warnings.warn(
-        "to_networkx_graph is deprecated and will be removed in version 4.0.0. "
-        + "Use MolGraph.to_networkx() instead.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return graph.to_networkx()
-
-
-def to_plotly_figure(graph: MolGraph) -> go.Figure:
-    """Deprecated: Use MolGraph.to_plotly() method instead."""
-    warnings.warn(
-        "to_plotly_figure is deprecated and will be removed in version 4.0.0. "
-        + "Use MolGraph.to_plotly() instead.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return graph.to_plotly()
 
 
 __all__ = [
---
+++ xyz2graph/helpers.py
@@ -0,0 +1,28 @@
+import warnings
+
+import networkx as nx
+import plotly.graph_objs as go
+
+from .xyz2graph import MolGraph
+
+
+def to_networkx_graph(graph: MolGraph) -> nx.Graph:
+    """Deprecated: Use MolGraph.to_networkx() method instead."""
+    warnings.warn(
+        "to_networkx_graph is deprecated and will be removed in version 4.0.0. "
+        + "Use MolGraph.to_networkx() instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return graph.to_networkx()
+
+
+def to_plotly_figure(graph: MolGraph) -> go.Figure:
+    """Deprecated: Use MolGraph.to_plotly() method instead."""
+    warnings.warn(
+        "to_plotly_figure is deprecated and will be removed in version 4.0.0. "
+        + "Use MolGraph.to_plotly() instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return graph.to_plotly()
